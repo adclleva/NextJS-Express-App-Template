364866c9e0064b7edd94beca1af95b1deb18bdc5
adclleva <arvin.lleva@yahoo.com>
2023-11-04 11:55:49 -0700

    added the client-app-router app

---
+++ client-app-router/app/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+export default function Home() {
+  const [message, setMessage] = useState("Loading...");
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/home");
+        const data = await response.json();
+        console.log({ data });
+
+        setMessage(data.message);
+        setPeople(data.people);
+      } catch (error) {
+        console.log(error);
+        setMessage("Error");
+        setPeople([]);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      {people.map((person, index) => (
+        <div key={index}>
+          <h2>{person}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
